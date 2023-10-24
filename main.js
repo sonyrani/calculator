5973357f543365fd7695e4eba1a6046edc99736a
Punam Kumari <114477143+sonyrani@users.noreply.github.com>
2023-10-24 12:29:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button");
+const specialChars=["%", "*", "/", "-", "+", "="];
+let output="";
+
+//Define function to calculate bases on button clicked.
+    const calculate =(btnValue)=> {
+    if(btnValue === "=" && btnValue !== ""){
+        //if output has '%', replace with '/100' before evaluating
+        output=eval(output.replace("%", "/100"));
+    }
+    else if (btnValue==="AC"){
+        output="";
+    }
+    else if(btnValue==="DEL"){
+        //if DEL button is clicked, remove the last character from the output
+        output=output.toString().slice(0, -1)
+    }
+    else{
+        //if output is empty and button is specialChars then return
+        if(output==="" && specialChars.includes(btnValue))return;
+        output +=btnValue;
+    }
+    display.value=output;
+};
+
+//Add event listener to buttons, call calculate() on click
+buttons.forEach((button) => {
+    //button click listner calls calculate() with dataset value as argument
+    button.addEventListener("click", (e) => calculate(e.target.dataset.value));
+});
